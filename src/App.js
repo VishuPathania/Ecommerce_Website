fe4c3134a0dd2b34385f07a68bde6f92642ecaab
VishuPathania <vishalpathania0003@gmail.com>
2023-08-06 20:56:44 +0530

    cart_comp_T2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Cart from './Cart';
 
 function App() {
+  const [showCart, setShowCart] = useState(false);
+
+  const toggleCart = () => {
+    setShowCart(!showCart);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Ecommerce Website</h1>
+        <button onClick={toggleCart}>Cart</button>
       </header>
+      <main>
+        {/* Your other components and content */}
+        {showCart && <Cart />}
+      </main>
     </div>
   );
 }
